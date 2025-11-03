a647db229b2dd027a98783c87e3b01031fbca241
sxtrng <sxtrng@gmail.com>
2025-11-03 06:35:29 -0500

    String Introduction

---
+++ FirstString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FirstString {
+    public static void main(String[] args) {
+
+        String stringVariable;
+
+        stringVariable = "This is a sentence."
+
+        System.out.println(stringVariable);
+        
+
+    }
+}
