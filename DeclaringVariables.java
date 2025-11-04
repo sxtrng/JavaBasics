9d2a1764a42427873e104d48e266cae624299c21
sxtrng <sxtrng@gmail.com>
2025-11-04 06:13:51 -0500

    variable basics

---
+++ DeclaringVariables.java
@@ -0,0 +1,31 @@
+public class DeclaringVariables {
+    public static void main(String[] args) {
+
+        System.out.println("A variable references a location in memory.");
+        System.out.println("Java is a statically and strongly typed language.");
+        System.out.println("All variables must be declared prior to use, and must include a name and a type.");
+        System.out.println("The compiler can detect mismatch between types and spelling errors.");
+        System.out.println("A variable declaration is a statement and must end with a semicolon.");
+        System.out.println("A variable name is an identifer.");
+        System.o.println("Use self-documenting names like 'numberOfSongs' rather than 'x' or 'n'.");
+
+        int x;
+        x = 10;
+        System.out.println("Value of x: " + x);
+
+        x = 50;
+        System.out.println("x has been given a new value: " + x);
+
+        int y = 100;
+        System.out.println("y +  was declared an initialized on one line.");
+        System.out.println("y = " + y);
+
+        //  Legal, but discouraged.
+        int a, b;
+        a = 500;
+        b = 700;
+        System.out.println(a + " " + b);
+
+        
+    }
+}
