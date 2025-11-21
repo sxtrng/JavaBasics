81476e399b99c83db9c55d96d32c495c0669cc42
sxtrng <sxtrng@gmail.com>
2025-11-20 21:48:50 -0500

    declaring and assigning

--- DeclaringVariables.java
+++ DeclaringVariables.java
@@ -4,11 +4,15 @@ public class DeclaringVariables {
         System.out.println("A variable references a location in memory.");
         System.out.println("Java is a statically and strongly typed language.");
         System.out.println("All variables must be declared prior to use, and must include a name and a type.");
+        System.out.println("Variable names begin with a letter or underscore, even a dollar sign.  Beginning with lower case is good practice.");
+        System.out.println("There are no spaces.  Use camel case for multipleWords.");
+        System.out.println("Numbers allowed, but not as a first letter.");
         System.out.println("The compiler can detect mismatch between types and spelling errors.");
         System.out.println("A variable declaration is a statement and must end with a semicolon.");
         System.out.println("A variable name is an identifer.");
+        System.out.println("Upper and lower case characters are distinct.");
         System.out.println("The = sign is the assignment operator.  The value is assigned to the variable name.");
-        System.o.println("Use self-documenting names like 'numberOfSongs' rather than 'x' or 'n'.");
+        System.out.println("Use self-documenting names like 'numberOfSongs' rather than 'x' or 'n'.");
 
         int x;
         x = 10;
