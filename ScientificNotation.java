71e5e727a3532828a858db25de450e343f85c110
sxtrng <sxtrng@gmail.com>
2026-07-21 19:50:12 -0400

    a look at scientific notation

---
+++ ScientificNotation.java
@@ -0,0 +1,17 @@
+public class ScientificNotation {
+
+	public static void main(String[] args) {
+		
+		double distanceToSun;
+		double massOfSun;
+
+		distanceToSun = 1.495979E11;
+		massOfSun = 1.989E30;
+
+		System.out.println(distanceToSun);
+		System.out.println(massOfSun);
+
+
+	}
+
+}
