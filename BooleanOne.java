04c153b9d73059e838e754b9273c1c1881bbe904
sxtrng <sxtrng@gmail.com>
2026-07-21 20:50:43 -0400

    declaring boolean variables

---
+++ BooleanOne.java
@@ -0,0 +1,12 @@
+public class BooleanOne {
+	public static void main(String[] args) {
+		
+
+		boolean b = true;
+		System.out.printf("%b%n", b);
+
+		b = false;
+		System.out.printf("%b%n", b);
+
+	}
+}
