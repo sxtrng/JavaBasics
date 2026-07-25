e1f5afb171d037964fccf437c1c8fc7762cfba4e
sxtrng <sxtrng@gmail.com>
2026-07-25 08:09:54 -0400

    Arithmetic Operators Intro

---
+++ OperatorsOne.java
@@ -0,0 +1,35 @@
+public class OperatorsOne {
+
+	public static void main(String[] args) {
+	
+		int a = 1;
+		int b = 2;
+
+		int c = a + b;
+		System.out.printf("%d + %d = %d%n", a, b, c);
+
+		c = c + 1;
+		System.out.printf("%d%n", c);
+
+		c++;
+		System.out.printf("%d%n", c);
+
+		c += 1;
+		System.out.printf("%d%n", c);
+
+		int d = 10;
+
+		c = c * 10 ;
+		System.out.printf("%d%n", c);
+
+		c /= 6;
+		System.out.printf("%d%n", c);
+
+		c -= 7;
+		System.out.printf("%d%n", c);
+
+
+
+
+	}
+}
