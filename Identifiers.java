eb0ca0612e854b67aae435576ae2ef2b9c3f1f82
sxtrng <sxtrng@gmail.com>
2026-07-29 06:22:30 -0400

    Identifiers

---
+++ Identifiers.java
@@ -0,0 +1,16 @@
+public class Identifiers {
+	public static void main(String[] args) {
+		
+
+		String identifiers = "And \'identifier\' is a name for an element, like a variable.";
+		System.out.println(identifiers);
+
+		identifiers = "It could also refer to a class and is named by the programmer.";
+		System.out.println(identifiers);
+
+		identifiers = "\'Identifiers\' can not be keywords, and begin with a letter.";
+		System.out.println(identifiers);
+
+
+	}
+}
