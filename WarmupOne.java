21925cd4bc206af78dbf5d71da9f4af813b97579
sxtrng <sxtrng@gmail.com>
2026-07-29 05:50:34 -0400

    Program to get started

---
+++ WarmupOne.java
@@ -0,0 +1,13 @@
+public class WarmUpOne {
+	public static void main(String[] args) {
+		
+		int number = 1;
+		String string = "Java";
+		char character = 'C';
+
+		System.out.printf("%s program number %d.%n", string, number);
+		System.out.printf("%s is a %c-like language.%n",	string, character);
+
+
+	}
+}
