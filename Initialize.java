b0cfa6704428ca86ee2e5598b730d6f3e0e8e80c
sxtrng <sxtrng@gmail.com>
2026-08-20 20:02:55 -0400

    Various ways to initialize variables

---
+++ Initialize.java
@@ -0,0 +1,29 @@
+public class Initialize {
+	public static void main(String[] args) {
+
+		int a;
+		int b;
+		int c;
+
+		a = 10;
+		b = 20;
+		c = 30;
+
+		System.out.printf("%n%d, %d, %d%n%n", a, b, c);
+
+		int d, e, f;
+
+		d = 40;
+		e = 50;
+		f = 60;
+
+		System.out.printf("%d, %d, %d%n%n", d, e, f);
+
+		int g = 70;
+		int h = 80;
+		int i = 90;
+
+		System.out.printf("%d, %d, %d%n%n", g, h, i);
+		
+	}
+}
